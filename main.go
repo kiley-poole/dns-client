64027daf1e32f9354c4c39120da9620a526af762
Kiley Poole <kpoole007@gmail.com>
2022-01-04 11:52:20 -0800

    Bound created socket and transmitted message.

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"syscall"
 )
 
 func main() {
+	sa := &syscall.SockaddrInet4{}
+	googleDns := &syscall.SockaddrInet4{
+		Addr: [4]byte{8, 8, 8, 8},
+		Port: 53,
+	}
+	var data []byte
 	socket, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_DGRAM, 0)
 	check(err)
-	fmt.Print(socket)
+	err = syscall.Bind(socket, sa)
+	check(err)
+	err = syscall.Sendto(socket, data, syscall.MSG_CONFIRM, googleDns)
+	check(err)
+	syscall.Close(socket)
 }
 
 func check(err error) {
