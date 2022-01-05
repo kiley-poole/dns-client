d29f06ce5efcd6013e91ba2aa8498bc9bbebd868
Kiley Poole <kpoole007@gmail.com>
2022-01-04 19:04:14 -0800

    working transmit DNS message with hardcoded host

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strings"
 	"syscall"
 )
 
@@ -10,14 +11,43 @@ func main() {
 		Addr: [4]byte{8, 8, 8, 8},
 		Port: 53,
 	}
-	var data []byte
+
+	DNSMessage := buildDNSMessage("google.com")
+
 	socket, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_DGRAM, 0)
 	check(err)
+
 	err = syscall.Bind(socket, sa)
 	check(err)
-	err = syscall.Sendto(socket, data, syscall.MSG_CONFIRM, googleDns)
+
+	err = syscall.Sendto(socket, DNSMessage, syscall.MSG_DONTWAIT, googleDns)
 	check(err)
-	syscall.Close(socket)
+
+	err = syscall.Close(socket)
+	check(err)
+}
+
+func buildDNSMessage(host string) []byte {
+	dnsHeader := []byte{0xFF, 0xFF, 0x01, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+	question := buildQuestion(host)
+
+	message := append(dnsHeader, question...)
+
+	return message
+}
+
+func buildQuestion(host string) []byte {
+	var question []byte
+	elements := strings.Split(host, ".")
+
+	for _, e := range elements {
+		l := len(e)
+		question = append(question, byte(l))
+		question = append(question, e...)
+	}
+	question = append(question, []byte{0x00, 0x00, 0x01, 0x00, 0x01}...)
+
+	return question
 }
 
 func check(err error) {
